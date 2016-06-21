7d93f12bc46614a5f9e1f9a7923933968c6c0236
Pirhoo <hello@pirhoo.com>
2016-06-21 17:42:59 +0200

    Added environement variables getter to get config

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
-
+from os import environ
 # Twitter API auth
-consumer_key = "boXa5KPQGFt0IpbOQV4OuyOBU"
-consumer_secret = "bQOlOM2cOXFsrZGfN580o9WlOL4REuJFjaDCdSdSfRb0TknKx2"
-access_token = "744906842862653444-1ji5tXpWD3pEt98hok3tSR4JCnI25PS"
-access_token_secret = "SDDxqFX0JkYWMbIfQakGRXFhDkoziCVO9NWVO5jv4jxPD"
-
+consumer_key = environ.get('CONSUMER_KEY', None)
+consumer_secret = environ.get('CONSUMER_SECRET', None)
+access_token = environ.get('ACCESS_TOKEN', None)
+access_token_secret = environ.get('ACCESS_TOKEN_SECRET', None)
