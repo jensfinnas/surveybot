f94ccb75b5db1f5e43117105c14058c9b86dcefd
jensfinnas <jens.finnas@gmail.com>
2016-06-21 17:29:25 +0100

    Rename

--- run.py
+++ run.py
@@ -10,22 +10,9 @@ x = TwitterBot("twine-parser/result.json",
     access_token_secret=access_token_secret,
     consumer_key=consumer_key,
     consumer_secret=consumer_secret)
-"""
-
-mentions = x.twitter.mentions_timeline()
 
 
-for mention in reversed(mentions):
-    print "*********"
-    print mention.user.screen_name, mention.text, x._has_reply(mention)
-    x.interpret_mention(mention)
-
-"""
-
-x.say(x.statements[-2])
-
-"""
 mentionlistener = MentionListener
 myStream = tweepy.Stream(auth = x.twitter.auth, listener=mentionlistener(x))
 myStream.filter(track=['askchartbot'])
-"""
+
